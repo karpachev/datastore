703573c9f66f5c90142ccdf4b1aa86a582605419
Vasil Karpachev <v.karpachev@gmail.com>
2016-10-24 23:34:26 +0300

    some small changes

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 var Datastore = require("./lib/datastore.js")
 
 
-var ds = new Datastore({ 
-  projectId : "zeta-bebop-142321"
+var ds = new Datastore({
+	projectId: "zeta-bebop-142321"
 });
 
 
 var Test = ds.KindWithSchema(
 	"Test",
-	[ 'page_id', 'period', 'date' ],
+	["page_id", "period", "date"],
 	[
-		{ 
-			name : "first_name",
-		 	type : Datastore.STRING 
+		{
+			name: "first_name",
+			type: Datastore.STRING
 		}
-		,{ 
-			name : "last_name",
-		 	type : Datastore.STRING 
+		, {
+			name: "last_name",
+			type: Datastore.STRING
 		}
-		,{ 
-			name : "age",
-		 	type : Datastore.INT,
-		 	excludeFromIndexes : true
+		, {
+			name: "age",
+			type: Datastore.INT,
+			excludeFromIndexes: true
 		}
 	]
 );
 
 var Insights = ds.KindWithSchema(
 	"Insights",
-	[ 'page_id', 'period', 'date' ],
+	["page_id", "period", "date"],
 	[
-		{ 
-			name : "name",
-		 	type : Datastore.STRING 
+		{
+			name: "name",
+			type: Datastore.STRING
 		}
-		,{ 
-			name : "age",
-		 	type : Datastore.INT,
-		 	excludeFromIndexes : true
+		, {
+			name: "age",
+			type: Datastore.INT,
+			excludeFromIndexes: true
 		}
 	]
 );
@@ -46,7 +46,7 @@ var t1 = new Test(["vasil", "karpachev", 13]);
 console.dir(t1);
 var t1_insights = new Insights(["vasil karpachev", 13]);
 console.dir(t1_insights);
-var t2_insights = new Insights({ name: "ivan petrov", age: 15});
+var t2_insights = new Insights({ name: "ivan petrov", age: 15 });
 console.dir(t2_insights.name);
 
 t1.save();
@@ -95,7 +95,7 @@ Test.get()
 
 var Map = new ds.Map(
 		"Insights",
-		[ 'page_id', 'period', 'date' ],
+		[ "page_id", "period", "date" ],
 		Datastore.INT
 	);
 
@@ -111,7 +111,7 @@ Map.get({
 Map.get({
 	page_id: 123123,
 	period: "day",
-	date: { '$gt' : "20160120" }
+	date: { "$gt" : "20160120" }
 }).then( records_db => { 
 });
 
